f64e9452dfa9a42b1ceb3c6a51190f474f5aa3ac
fhj <307890+freddyh@users.noreply.github.com>
2019-10-16 21:23:26 -0700

    installed puppeteer, running example code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://genius.com/2pac-keep-ya-head-up-lyrics');
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+})();
