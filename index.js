7c4db3579516fe94e347b9e7b1548231511471ad
fhj <307890+freddyh@users.noreply.github.com>
2019-10-16 22:04:34 -0700

    read text from lyrics div

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true
+  });
   const page = await browser.newPage();
   await page.goto('https://genius.com/2pac-keep-ya-head-up-lyrics');
-  await page.screenshot({ path: `screenshots/${Date()}.png` });
+
+  const lyrics_selector = 'body > routable-page > ng-outlet > song-page > div > div > div.song_body.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > defer-compile:nth-child(2) > lyrics > div > div > section';
+  const lyrics = await page.$eval(lyrics_selector, el => el.innerText);
+  console.log(lyrics);
+
   await browser.close();
 })();
