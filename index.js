81a4c78efe2bb705c6098784280735834c14cb25
fhj <307890+freddyh@users.noreply.github.com>
2020-03-05 22:37:59 -0800

    refactored selectors, prevent extra wait time after max errors are reached

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const showAllAlbumsSelector = 'div.full_width_button:nth-child(2)';
 const showAlbumSelector = '.modal_window-content > ng-transclude:nth-child(1) > scrollable-data:nth-child(1) > div:nth-child(1) > transclude-injecting-local-scope:nth-child(1) > div:nth-child(1) > mini-album-card:nth-child(1) > a:nth-child(1)';
 const albumTitleSelector = '.modal_window-content > ng-transclude:nth-child(1) > scrollable-data:nth-child(1) > div:nth-child(1) > transclude-injecting-local-scope:nth-child(1) > div:nth-child(1) > mini-album-card:nth-child(1) > a:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)';
 const albumYearSelector = '.modal_window-content > ng-transclude:nth-child(1) > scrollable-data:nth-child(1) > div:nth-child(1) > transclude-injecting-local-scope:nth-child(1) > div:nth-child(1) > mini-album-card:nth-child(1) > a:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)';
+const lyricsSelector = 'body > routable-page > ng-outlet > song-page > div > div > div.song_body.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > defer-compile:nth-child(2) > lyrics > div > div > section';
+const showAllSongsSelector = 'body > routable-page > ng-outlet > routable-profile-page > ng-outlet > routed-page > profile-page > div.column_layout > div.column_layout-column_span.column_layout-column_span--primary > artist-songs-and-albums > div.full_width_button.u-clickable.u-bottom_margin';
+const scrollable_section = 'body > div.modal_window';
 
 const buildItemSelector = (n) => {
     return `body > div.modal_window > div.modal_window-content.modal_window-content--narrow_width.modal_window-content--white_background > ng-transclude > artist-songs > scrollable-data > div:nth-child(1) > transclude-injecting-local-scope:nth-child(${n}) > div > mini-song-card > a`;
@@ -53,9 +56,7 @@ const readLyrics = async (songUrl) => {
     console.log(`Opening page at ${songUrl}`);
     const task = await openPageTask(songUrl);
     const page = task.page;
-
-    const lyrics_selector = 'body > routable-page > ng-outlet > song-page > div > div > div.song_body.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > defer-compile:nth-child(2) > lyrics > div > div > section';
-    const lyrics = await page.$eval(lyrics_selector, el => el.innerText);
+    const lyrics = await page.$eval(lyricsSelector, el => el.innerText);
     await task.onComplete();
     return lyrics;
 };
@@ -67,8 +68,7 @@ const readSongUrls = async (artistUrl) => {
     const page = task.page;
     console.log(`Opened page at ${artistUrl}`);
 
-    const view_all_songs_selector = 'body > routable-page > ng-outlet > routable-profile-page > ng-outlet > routed-page > profile-page > div.column_layout > div.column_layout-column_span.column_layout-column_span--primary > artist-songs-and-albums > div.full_width_button.u-clickable.u-bottom_margin';
-    await page.click(view_all_songs_selector);
+    await page.click(showAllSongsSelector);
     await page.screenshot({ path: `screenshots/${Date()}.png` });
     console.log(`Waiting for ten seconds...`);
     await page.waitFor(10 * 1000);
@@ -80,9 +80,9 @@ const readSongUrls = async (artistUrl) => {
     var songs = [];
     while (error == null) {
         try {
-            const itemSelector = `body > div.modal_window > div.modal_window-content.modal_window-content--narrow_width.modal_window-content--white_background > ng-transclude > artist-songs > scrollable-data > div:nth-child(1) > transclude-injecting-local-scope:nth-child(${n}) > div > mini-song-card > a`;
+            const itemSelector = buildItemSelector(n);
             const songUrl = await page.$eval(itemSelector, el => el.href);
-            const songNameSelector = `.modal_window-content > ng-transclude:nth-child(1) > artist-songs:nth-child(1) > scrollable-data:nth-child(3) > div:nth-child(1) > transclude-injecting-local-scope:nth-child(${n}) > div:nth-child(1) > mini-song-card:nth-child(1) > a:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)`;
+            const songNameSelector = buildSongNameSelector(n);
             const songName = await page.$eval(songNameSelector, el => el.innerHTML);
 
             const songData = {
@@ -103,21 +103,20 @@ const readSongUrls = async (artistUrl) => {
                 const message = 'Max Failures Reached';
                 error = Error(message);
                 console.error(error);
-            }
-            //  else {
-            console.log(`Scrolling down the page...preparing to try again`);
-            const scrollable_section = 'body > div.modal_window';
-            await page.evaluate(selector => {
-                const scrollableSection = document.querySelector(selector);
-                const offset = 1000000;
-                scrollableSection.scrollTop = offset;
-            }, scrollable_section);
+            } else {
+                console.log(`Scrolling down the page...preparing to try again`);
+                // const scrollable_section = 'body > div.modal_window';
+                await page.evaluate(selector => {
+                    const scrollableSection = document.querySelector(selector);
+                    const offset = 1000000;
+                    scrollableSection.scrollTop = offset;
+                }, scrollable_section);
 
-            const interval = 6;
-            console.log(`Waiting for ${interval} seconds`);
-            await page.waitFor(interval * 1000);
-            await page.screenshot({ path: `screenshots/${Date()}.png` });
-            // }
+                const interval = 6;
+                console.log(`Waiting for ${interval} seconds`);
+                await page.waitFor(interval * 1000);
+                await page.screenshot({ path: `screenshots/${Date()}.png` });
+            }
         }
     }
 
